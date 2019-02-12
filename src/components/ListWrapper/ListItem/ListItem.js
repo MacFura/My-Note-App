276fd2087b552ed8974d1772fc6da88b56480bec
VanDev <maciek.fura@gmail.com>
2019-02-12 02:34:17 +0100

    Extract Title Component

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ListItem.module.scss";
 import Button from '../../Button/Button'
+import Title from "../../Title/Title";
 
 const ListItem = ({ image, name, description, twitterLink }) => {
 
@@ -11,7 +12,7 @@ const ListItem = ({ image, name, description, twitterLink }) => {
     <li className={styles.wrapper}>
       <ImageTag src={image} className={image ? styles.image : styles.imageNone} alt={name} />
       <div>
-        <h2 className={styles.name}>{name}</h2>
+        <Title>{name}</Title>
         <p className={styles.description}>{description}</p>
         <Button
           href={twitterLink}
